05f85baa88e1bc685e2ecb07795c7d459ca9d49f
Lydeon <leonlee190@gmail.com>
2018-07-03 20:00:06 -0700

    testing
    Checking integer, date, time

--- phonebill/src/main/java/edu/pdx/cs410J/seung2/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/seung2/Project1.java
@@ -14,58 +14,98 @@ public class Project1 {
       System.out.println(arg);
     }
 
-    if(args.length == 7){
-      System.out.println("\nTesting");
-      String delims = "[ //-]";
-      String[] name = args[0].split(delims);
-      String[] caller = args[1].split(delims);
-      String[] callee = args[2].split(delims);
-      String[] start = args[3].split(delims);
-      String[] startTime = args[4].split(":");
-      String[] end = args[5].split(delims);
-      String[] endTime = args[6].split(":");
+    if(args[0].equals("-print") || args[0].equals("-PRINT")){
+      System.out.println("Called Print");
+    }
+    if(args[0].equals("-readme") || args[0].equals("-README")){
+      System.out.println("Called README");
+    }
+    else if(args.length == 7){
+      String[] name = args[0].split(" ");
 
-      for(String arg : name){
-        System.out.println(arg);
-      }
-      for(String arg : caller){
-        System.out.println(arg);
-      }
-      for(String arg : callee){
-        System.out.println(arg);
-      }
-      for(String arg : start){
-        System.out.println(arg);
-      }
-      for(String arg : startTime){
-        System.out.println(arg);
-      }
-      for(String arg : end){
-        System.out.println(arg);
-      }
-      for(String arg : endTime){
-        System.out.println(arg);
-      }
+      String[] caller = args[1].split("-");
+      checkInt(caller, "Caller");
+
+      String[] callee = args[2].split("-");
+      checkInt(callee, "Callee");
+
+      String[] start = args[3].split("/");
+      checkInt(start, "starting Date");
+      checkDate(start, "starting time's");
+
+      String[] startTime = args[4].split(":");
+      checkInt(startTime, "starting Time");
+      checkTime(startTime, "starting Time");
+
+      String[] end = args[5].split("/");
+      checkInt(end, "ending Date");
+      checkDate(start, "ending time's");
+
+      String[] endTime = args[6].split(":");
+      checkInt(endTime, "ending Time");
+      checkTime(endTime, "ending Time");
     }
     else{
-      System.err.println("Missing command line arguments");
+      System.err.println("Incorrect command line argument");
+      System.exit(1);
     }
 
     System.exit(1);
   }
 
-  public void checkName(String caller){
-    if(caller.length > 3 || caller.length < 0){
-      System.err.println("Invalid caller name");
+  public static boolean isInt(String input){
+    boolean isIt;
+
+    try{
+      Integer.parseInt(input);
+      isIt = true;
+    }catch(NumberFormatException e){
+      isIt = false;
     }
 
-    if(caller.length == 1){
-      System.err.println("Missing First or Last name");
+    return isIt;
+  }
+
+  public static void checkInt(String[] args, String name){
+    for(String arg : args){
+      boolean checker = isInt(arg);
+
+      if(checker == false){
+        System.err.println("Incorrect " + name + " integer value!");
+        System.exit(1);
+      }
     }
   }
 
-  public void checkNum(String number){
+  public static void checkDate(String[] args, String name) {
+    int month = Integer.parseInt(args[0]);
+    int day = Integer.parseInt(args[1]);
 
+    if (month < 1 || month > 12) {
+      System.err.println("Incorrect " + name + " month value!");
+      System.exit(1);
+    }
+    if (day < 1 || day > 31) {
+      System.err.println("Incorrect " + name + " date value!");
+      System.exit(1);
+    }
+    if (args[2].length() != 4) {
+      System.err.println("Incorrect " + name + " year value!");
+      System.exit(1);
+    }
   }
 
+  public static void checkTime(String[] args, String name){
+    int hour = Integer.parseInt(args[0]);
+    int min = Integer.parseInt(args[1]);
+
+    if(hour < 0 || hour > 24){
+      System.err.println("Incorrect " + name + " hour value!");
+      System.exit(1);
+    }
+    if(min < 0 || min >= 60){
+      System.err.println("Incorrect " + name + " minute value!");
+      System.exit(1);
+    }
+  }
 }
