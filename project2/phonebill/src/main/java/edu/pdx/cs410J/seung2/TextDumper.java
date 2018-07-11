7e91aa49268fc5e7979f7faa15340d6615c6c456
Lydeon <leonlee190@gmail.com>
2018-07-11 05:33:33 -0700

    implement
    implements the interfaces

--- project2/phonebill/src/main/java/edu/pdx/cs410J/seung2/TextDumper.java
+++ project2/phonebill/src/main/java/edu/pdx/cs410J/seung2/TextDumper.java
@@ -1,7 +1,14 @@
 package edu.pdx.cs410J.seung2;
 
+import edu.pdx.cs410J.AbstractPhoneBill;
 import edu.pdx.cs410J.PhoneBillDumper;
 
-public class TextDumper {
+import java.io.IOException;
 
+public class TextDumper implements PhoneBillDumper{
+
+    @Override
+    public void dump(AbstractPhoneBill abstractPhoneBill) throws IOException {
+
+    }
 }
--- project2/phonebill/src/main/java/edu/pdx/cs410J/seung2/TextParser.java
+++ project2/phonebill/src/main/java/edu/pdx/cs410J/seung2/TextParser.java
@@ -1,6 +1,16 @@
 package edu.pdx.cs410J.seung2;
 
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.ParserException;
 import edu.pdx.cs410J.PhoneBillParser;
 
-public class TextParser {
+public class TextParser implements PhoneBillParser{
+
+    @Override
+    public AbstractPhoneBill parse() throws ParserException {
+        PhoneBill bill;
+
+
+        return null;
+    }
 }
